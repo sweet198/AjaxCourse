179a64a8b01b603a0be5210c9008e900b9d7f3c5
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-11-11 22:11:12 +0300

    made with fetch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
-    function req() {
+    // outdated method
+    /*function req() {
         const request = new XMLHttpRequest();
         request.open('GET', 'http://localhost:3000/people');
         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
@@ -37,6 +38,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
         this.remove(); // remove button
     }
+    document.querySelector('button').addEventListener('click', req, {'once': true});*/
+
+    function req() {
+        getResource('http://localhost:3000/people')
+            .then(data => createCards(data))
+            .catch(err => console.error(err));
+
+        this.remove(); // remove button
+    }
     document.querySelector('button').addEventListener('click', req, {'once': true});
 
+    async function getResource(url) {
+        const res = await fetch(`${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    function createCards(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            let icon;
+            if (item.sex === 'male') {
+                icon = "icons/mars.png"
+            } else {
+                icon = "icons/female.png"
+            }
+
+            card.innerHTML = `
+                        <img src="${item.photo}" alt="">
+                        <div class="name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="male">
+                        </div>
+                   `;
+            document.querySelector('.app').append(card);
+        })
+    }
 });
