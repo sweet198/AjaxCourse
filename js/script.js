e9c894d90694ee6d5998bdc69178c7f95a0f69df
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-11-12 08:28:22 +0300

    plugged axios

--- js/script.js
+++ js/script.js
@@ -40,7 +40,9 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     document.querySelector('button').addEventListener('click', req, {'once': true});*/
 
-    function req() {
+
+    // modern method
+    /*function req() {
         getResource('http://localhost:3000/people')
             .then(data => createCards(data))
             .catch(err => console.error(err));
@@ -56,6 +58,48 @@ window.addEventListener('DOMContentLoaded', () => {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         return await res.json();
+    }
+
+    function createCards(response) {
+        response.forEach(item => {
+            let card = document.createElement('div');
+
+            card.classList.add('card');
+
+            let icon;
+            if (item.sex === 'male') {
+                icon = "icons/mars.png"
+            } else {
+                icon = "icons/female.png"
+            }
+
+            card.innerHTML = `
+                        <img src="${item.photo}" alt="">
+                        <div class="name">${item.name} ${item.surname}</div>
+                        <div class="sex">
+                            <img src=${icon} alt="male">
+                        </div>
+                   `;
+            document.querySelector('.app').append(card);
+        })
+    }*/
+
+    function req() {
+        getResource('http://localhost:3000/people')
+            .then(data => createCards(data.data))
+            .catch(err => console.error(err));
+
+        this.remove(); // remove button
+    }
+    document.querySelector('button').addEventListener('click', req, {'once': true});
+
+    async function getResource(url) {
+        const res = await axios(`${url}`);
+
+        if (res.status !== 200) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return res;
     }
 
     function createCards(response) {
